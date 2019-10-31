99919338ddf3d761679ca30be4cab5145f8992ab
makieas <ashenafimenza@gmail.com>
2019-10-31 14:36:33 +0100

    create(co2mpas_driver): create co2mpas-driver package.

--- build/lib/co2mpas_driver/model/__init__.py
+++ build/lib/co2mpas_driver/model/__init__.py
@@ -17,6 +17,7 @@ Sub-Modules:
 """
 import numpy as np
 import schedula as sh
+from .driver import Driver as dr
 from co2mpas_driver.model.co2mpas import get_full_load, \
     calculate_full_load_speeds_and_powers, calculate_full_load_torques
 from co2mpas_driver.generic_co2mpas import light_co2mpas_series, \
@@ -32,13 +33,10 @@ dsp.add_func(
     calculate_full_load_torques,
     outputs=['full_load_torques']
 )
+
 dsp.add_func(
     light_co2mpas_instant,
     outputs=['fc']
-)
-dsp.add_func(
-    light_co2mpas_series,
-    outputs=['fp']
 )
 
 
@@ -733,68 +731,105 @@ def define_times(duration, sim_step):
     return np.arange(0, duration + sim_step, sim_step)
 
 
+@sh.add_function(dsp, outputs=['driver_simulation_model'])
+def define_driver_simulation_model(transmission, gs, curves, driver_style):
+    from .driver import Driver
+    return Driver(transmission, gs, curves, driver_style)
+
+
 @sh.add_function(dsp, outputs=['gears', 'velocities'])
-def run_simulation(transmission, starting_speed, gs, times, curves,
-                   desired_velocity, driver_style):
+def run_simulation(
+        driver_simulation_model, starting_velocity, times, desired_velocity):
     """
     Run simulation.
 
-    :param transmission:
-        Transmission type of vehicle.
-    :type transmission: str
+    :param driver_simulation_model:
+        Driver simulation model.
+    :type driver_simulation_model:
 
-    :param starting_speed:
+    :param starting_velocity:
         Current speed.
-    :type starting_speed: int
-
-    :param gs: list
-        Gear shifting style.
-    :type gs: int
+    :type starting_velocity: int
 
     :param times:
         Sample time series.
     :type times: np.array
 
-    :param curves: list
-        Final acceleration curves.
-    :type curves: list
-
     :param desired_velocity:
         Desired velocity.
     :type desired_velocity: int
-
-    :param driver_style:
-        Driving style.
-    :type driver_style: int
 
     :return:
         Gears & velocities.
     :rtype: int, list
     """
-    from .simulation import (
-        gear_for_speed_profiles, accMFC, correct_acc_clutch_on
-    )
-    velocities = [starting_speed]
-
-    speed = starting_speed
-
-    # Returns the gear that must be used and the clutch condition
-    gear, gear_count = gear_for_speed_profiles(gs, speed, 0, 0)
-    gear_count = 0
-    gears = [gear]
-
-    # Core loop
-    for dt in np.diff(times):
-        gear, gear_count = gear_for_speed_profiles(gs, speed, gear, gear_count)
-        acc = accMFC(
-            speed, driver_style, desired_velocity, curves[gear - 1]
-        )
-        speed += correct_acc_clutch_on(gear_count, acc, transmission) * dt
-
-        # Gather data
-        gears.append(gear)
-        velocities.append(speed)
-    return gears, velocities
+    model = driver_simulation_model.reset(starting_velocity)
+    r = [(model._gear, starting_velocity, 0)]  # Gather data
+    r.extend(model(dt, desired_velocity) for dt in np.diff(times))
+    return list(zip(*r))[:2]
+# @sh.add_function(dsp, outputs=['gears', 'velocities'])
+# def run_simulation(transmission, starting_velocity, gs, times, curves,
+#                    desired_velocity, driver_style):
+#     """
+#     Run simulation.
+#
+#     :param transmission:
+#         Transmission type of vehicle.
+#     :type transmission: str
+#
+#     :param starting_velocity:
+#         Current speed.
+#     :type starting_velocity: int
+#
+#     :param gs: list
+#         Gear shifting style.
+#     :type gs: int
+#
+#     :param times:
+#         Sample time series.
+#     :type times: np.array
+#
+#     :param curves: list
+#         Final acceleration curves.
+#     :type curves: list
+#
+#     :param desired_velocity:
+#         Desired velocity.
+#     :type desired_velocity: int
+#
+#     :param driver_style:
+#         Driving style.
+#     :type driver_style: int
+#
+#     :return:
+#         Gears & velocities.
+#     :rtype: int, list
+#     """
+#     from .simulation import (
+#         gear_for_speed_profiles, accMFC, correct_acc_clutch_on
+#     )
+#     velocities = [starting_velocity]
+#
+#     velocity = starting_velocity
+#
+#     # Returns the gear that must be used and the clutch condition
+#     gear = gear_for_speed_profiles(gs, velocity, 0, 0)[0]
+#     gear_count = 0
+#     gears = [gear]
+#
+#     # Core loop
+#     for dt in np.diff(times):
+#         gear, gear_count = gear_for_speed_profiles(gs, velocity, gear,
+#                                                    gear_count)
+#         acc = accMFC(
+#             velocity, driver_style, desired_velocity, curves[gear - 1]
+#         )
+#         velocity += correct_acc_clutch_on(gear_count, acc, transmission) * dt
+#
+#         # Gather data
+#         gears.append(gear)
+#         velocities.append(velocity)
+#     return gears, velocities
 
 
 @sh.add_function(dsp, outputs=['accelerations'])
---
+++ build/lib/co2mpas_driver/model/driver.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+
+
+class Driver:
+    """
+        Blueprint for driver.
+    """
+
+    def __init__(self, transmission, gs, curves, driver_style):
+        self.transmission = transmission
+        self.gs = gs
+        self.curves = [None] + list(curves)
+        self.driver_style = driver_style
+        self._velocity = self._gear_count = self._gear = None
+
+    def reset(self, starting_velocity):
+        from .simulation import gear_for_speed_profiles as func
+        self._gear = func(self.gs, starting_velocity, 0, 0)[0]
+        self._gear_count, self._velocity = 0, starting_velocity
+        return self
+
+    def __call__(self, dt, desired_velocity, update=True):
+        from .simulation import (
+            gear_for_speed_profiles, accMFC, correct_acc_clutch_on
+        )
+        g, gc = gear_for_speed_profiles(
+            self.gs, self._velocity, self._gear, self._gear_count
+        )
+
+        a = correct_acc_clutch_on(gc, accMFC(
+            self._velocity, self.driver_style, desired_velocity, self.curves[g]
+        ), self.transmission)
+        v = self._velocity + a * dt
+        if update:
+            self._gear, self._gear_count, self._velocity = g, gc, v
+        return g, v, a
