9dbd1d80da3ee03c461225720115f7bed8ffadc7
makieas <ashenafimenza@gmail.com>
2019-11-05 09:50:27 +0100

    create(co2mpas_driver): Create co2mpas driver package.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
                     'dynamics accurately and consistently',
         long_description=readme(),
         long_description_content_type='text/markdown',
-        project_urls={"Sources": "https://github.com/ashenafimenza/new_MFC"},
+        project_urls={"Sources": "https://github.com/ashenafimenza/co2mpas_driver"},
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
